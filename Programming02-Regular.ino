8fb2e96d625a3f7eedc64187aa9e494353e41482
gzhou70 <gzhou70@stuy.edu>
2024-10-10 17:18:56 -0400

    Update Programming02-Regular.ino

--- Programming02-Regular.ino
+++ Programming02-Regular.ino
@@ -2,20 +2,28 @@
 //Function that returns x^2
 int pow2(int x) {
   //Your code here
+ return x*x;
 }
 
 //Function that gives back the average of an array
 float avg(float *a, int a_size) {
   //Your code here
+  total=0;
+  for(int i=0; i < a_size;i++) {
+    total = total +*a[i]
+  }
+  return total / a_size
 }
 
 //Given a pointer, change the value that the pointer references to 42069
 void changeX(int *x_p) {
   //Your code here
+  *x_p = 42069;
 }
 
 float getSlope(float x1, float y1, float x2, float y2) {
   //Your code here
+  return (y2-y1)/(x2-x1);
 }
 
 //CHALLENGE:
